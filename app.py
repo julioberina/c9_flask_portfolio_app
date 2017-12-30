452e54bccaa76af20a2735b6c016a1d713c3ccae
Julio Berina <julioberina@gmail.com>
2017-12-30 14:43:19 -0800

    Implemented a number guessing game to replace Time

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect
 import datetime
-import pytz # timezone 
+import pytz # timezone
 import requests
 import os
+import random
 
-
-
+# for Number Guess game
+number = random.randrange(1, 101)
 app = Flask(__name__)
 
 
@@ -44,40 +45,42 @@ def shopping_list_post():
       return render_template('shopping_list.html')
     elif request.method == 'POST':
           print(request.form['text'].split())
-          
+
           shop_list = []
           try:
             for item in request.form['text'].split():
-              
+
               shop_list.append(item)
 
-              
-              
+
+
             return render_template('shopping_list.html', result="\n".join([str(item) for item in shop_list]))
           except ValueError:
             return "Easy now! Let's keep it simple! Just words with a space between them"
-          
-  	      
-@app.route('/time', methods=['GET','POST'])
-def time_post():
-    # --> ['5', '6', '8']
-    # print(type(request.form['text']))
 
-    if request.method == 'GET':
-        return render_template('time.html')
-    elif request.method == 'POST':
-          print(request.form['text'].split())
-          
-          for item in request.form['text'].split():
-            answer = (datetime.datetime.now(pytz.timezone("Europe/Dublin")).strftime('Time = ' + '%H:%M:%S' + ' GMT ' + ' Year = ' + '%d-%m-%Y'))
-            #answer = datetime.datetime.now().strftime('Time == ' + '%H:%M:%S' + ' Year == ' + '%d-%m-%Y')
-            #answer = datetime.datetime.now().strftime('%Y-%m-%d \n %H:%M:%S')
 
-              
-              
-            return render_template('time.html', result=answer)
+@app.route('/number_guess', methods=['GET','POST', 'PUT'])
+def number_guess_post():
+	global number
+	if request.method == 'GET':
+		return render_template('number_guess.html')
+	elif request.method == 'POST':
+		print(int(request.form['text']))
+		consensus = ''
 
-         
+		try:
+			guess = int(request.form['text'])
+			if guess < number: consensus = 'Guess higher!'
+			elif guess > number: consensus = 'Guess lower!'
+			else:
+				consensus = 'You got it! I have a new number now...'
+				number = random.randrange(1, 101)
+		except ValueError:
+			return "Easy now! Let's keep it simple! Numbers only or don't leave blank!"
+
+		return render_template('number_guess.html', result=consensus)
+
+
 
 @app.route('/python_apps')
 def python_apps_page():
